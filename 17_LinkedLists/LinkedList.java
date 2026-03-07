d09fb666dbe13839bbaeba09ff8a25e0a587a5e1
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-03-07 22:07:18 +0530

    Creation of LinkedList, Head and Tail in LL, Add first/last in LL, Print LL, Add in middle, Size of LL, Remove first/last in LL

---
+++ 17_LinkedLists/LinkedList.java
@@ -0,0 +1,173 @@
+public class LinkedList {
+    
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    //Size of a Linkedlist
+    public static int size;
+
+    //Methods-> Operation to be performed on LL is done by methods
+    public void addFirst(int data){
+        //Step 1-> create new node
+        Node newNode = new Node(data);
+        size++;
+        //Base case->assuming LL is empty -> first create a newNode
+         if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //Step 2-> newNode next = head;
+        newNode.next = head; //Link
+
+        //Step 3-> Update head value -> head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+
+        //Step 1-> create a new Node
+        Node newNode = new Node(data);
+        size++;
+        //Base case -> If LL is empty
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        //Step 2-> Tail next = newNode
+        tail.next = newNode;
+
+        //Step 3-> tail = newNode
+        tail = newNode;
+    }
+
+    //print method
+    public void print(){
+        //Base case-> LL is empty
+        if(head == null){
+            System.err.println("LinkedList is Empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.err.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.err.println("null");
+    }
+
+    //add in the middle
+    public void add(int idx, int data){
+        //Special Case
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        // i = idx-1; temp-> prev
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    //removeFirst method->return the value of node which is deleting
+    public int removeFirst(){
+        //Special case
+        //case 1-> LL is empty
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+        //Case 2-> If Head and Tail pointing to same node means it is a single node
+        else if(size == 1){
+            int val = head.data;
+            //change both head and tail
+            head = tail = null;
+            //size also changes
+            size = 0;
+
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        //removing node so size changes
+        size--;
+        return val;
+    }
+
+    //removeLast method -> delete tail node, return the value of that node
+    public int removeLast(){
+        //Case-1 If LL is empty
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }
+        //Case 2-> If Head and Tail pointing to same node means it is a single node
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        //prev node index: i = size-2
+        Node prev = head;
+        for(int i=0;i<size-2; i++){
+            prev = prev.next;
+        }
+        int val = prev.next.data; //tail.data
+        prev.next = null;
+        tail = prev;
+        //size changes if removig from last
+        size--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+            // write your code here
+            LinkedList ll = new LinkedList();
+            ll.print();
+            ll.addFirst(2);
+            ll.print();
+            ll.addFirst(1);
+            ll.print();
+            ll.addLast(4);
+            ll.print();
+            ll.addLast(5);
+            ll.add(2,3);
+            ll.print();
+
+            System.out.println("Linkedlist_Size: "+ ll.size);
+
+            ll.removeFirst();
+            ll.print();
+
+            System.out.println("Linkedlist_Size: "+ ll.size);
+
+            ll.removeLast();
+            ll.print();
+            System.out.println("Linkedlist_Size: "+ ll.size);
+        }
+    }
+
+
+
