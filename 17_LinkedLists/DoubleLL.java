acb4b0c2ca54f9a8f0994a5c66414802cabee268
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-03-11 23:14:32 +0530

    removeFirst method in DoubleLL

--- 17_LinkedLists/DoubleLL.java
+++ 17_LinkedLists/DoubleLL.java
@@ -38,12 +38,38 @@ public class DoubleLL {
         System.out.println("null");
     }
 
+    //removeFirst
+    public int removeFirst(){
+        //base case
+        if(head == null){
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+        //DLL is singleNode
+        if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
     //main
     public static void main(String[] args) {
         DoubleLL dll = new DoubleLL();
         dll.addFirst(3);
         dll.addFirst(2);
         dll.addFirst(1);
+        dll.print();
+        System.out.println(dll.size);
+
+        dll.removeFirst();
         dll.print();
         System.out.println(dll.size);
     }
