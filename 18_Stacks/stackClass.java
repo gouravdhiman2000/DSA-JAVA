3e55a3378948c1a77766e81611e768cae0f5a4ad
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-03-26 13:31:21 +0530

    Stack Implementatin using LinkedList

--- 18_Stacks/stackClass.java
+++ 18_Stacks/stackClass.java
@@ -1,39 +1,86 @@
 import java.util.*;
 public class stackClass {
-    static class stack{
-        static ArrayList<Integer> list = new ArrayList<>();
+    // static class stack{
+    //     static ArrayList<Integer> list = new ArrayList<>();
 
-        //check if stack is empty or not
-        public static boolean isEmpty(){
-            return list.size() == 0;
-        }
+    //     //check if stack is empty or not
+    //     public static boolean isEmpty(){
+    //         return list.size() == 0;
+    //     }
 
-        //push O(1)
-        public static void push(int data){
-            list.add(data);
-        }
+    //     //push O(1)
+    //     public static void push(int data){
+    //         list.add(data);
+    //     }
 
-        //pop O(1)
-        public static int pop(){
-            //corner case
-            if(isEmpty()){
-                return -1;
+    //     //pop O(1)
+    //     public static int pop(){
+    //         //corner case
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         int top = list.get(list.size()-1); //get- O(1)
+    //         list.remove(list.size()-1); //removen - O(1)
+    //         return top;
+    //     }
+
+    //     //peek O(1)
+    //     public static int peek(){
+    //         if(isEmpty()){
+    //             return -1;
+    //         }
+    //         return list.get(list.size()-1);
+    //     }
+    // }
+        static class Node{
+            int data;
+            Node next;
+            Node(int data){
+                this.data = data;
+                this.next = null;
             }
-            int top = list.get(list.size()-1); //get- O(1)
-            list.remove(list.size()-1); //removen - O(1)
-            return top;
         }
 
-        //peek O(1)
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
+        static class stack{
+            static Node head = null;
+
+            public static boolean isEmpty(){
+                return head == null;
             }
-            return list.get(list.size()-1);
+
+            //push
+            public static  void push(int data){
+                Node newNode = new Node(data);
+                if(isEmpty()){
+                    head = newNode;
+                    return;
+                }
+                newNode.next = head;
+                head = newNode;
+            }
+
+            //pop
+            public static int pop(){
+                if(isEmpty()){
+                    return -1;
+                }
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+
+            //peek
+            public static int peek(){
+                if(isEmpty()){
+                    return -1;
+                }
+                return head.data;
+            }
         }
-    }
+
+   
     public static void main(String[] args) {
-        Stack s = new Stack();
+        stack s = new stack();
         s.push(1);
         s.push(2);
         s.push(3);
