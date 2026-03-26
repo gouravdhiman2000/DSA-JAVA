9d1301f64acf13880ec7df64adac534903a5d6cf
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-03-26 20:12:31 +0530

    Push at bottom of Stack

--- 18_Stacks/stackClass.java
+++ 18_Stacks/stackClass.java
@@ -78,6 +78,15 @@ public class stackClass {
         //     }
         // }
 
+    public static void pushAtBottom(Stack<Integer> s, int data){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
    
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
@@ -85,9 +94,11 @@ public class stackClass {
         s.push(2);
         s.push(3);
 
+        pushAtBottom(s, 4);
+
         while(!s.isEmpty()){
-            System.out.println(s.peek());
-            s.pop();
+            System.out.println(s.pop());
+            // s.pop();
         }
     }
 }
