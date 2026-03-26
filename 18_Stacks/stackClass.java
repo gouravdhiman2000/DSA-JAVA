a0e130150cf62af5fc26975fc8af6e28e8f0a782
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-03-26 12:31:05 +0530

    Stack Implementation using ArrayList with  Operations -> Push, Pop, Peek, isEmpty

---
+++ 18_Stacks/stackClass.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class stackClass {
+    static class stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        //check if stack is empty or not
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push O(1)
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop O(1)
+        public static int pop(){
+            //corner case
+            if(isEmpty()){
+                return -1;
+            }
+            int top = list.get(list.size()-1); //get- O(1)
+            list.remove(list.size()-1); //removen - O(1)
+            return top;
+        }
+
+        //peek O(1)
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
