df451dd4eb1951c955e6c758d796a80e1ae839bb
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-05-21 11:47:46 +0530

    Actvity Selection -> if end[] is not sorted

--- 20_Greedy_Algorithms/activity_Selection.java
+++ 20_Greedy_Algorithms/activity_Selection.java
@@ -1,28 +1,41 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class activity_Selection {
     public static void main(String[] args) {
         int start[] = {1,3,0,5,8,5};
         int end[] = {2,4,6,7,9,9};
 
+        //sorting if end[] is not sorted
+        int act[][] = new int[start.length][3];
+        for(int i=0; i<start.length;i++){
+            act[i][0] = i;
+            act[i][1] = start[i];
+            act[i][2] = end[i];
+        }
+        //lamda fxn -> shortform of func.
+        Arrays.sort(act, Comparator.comparingDouble( o -> o[2]));
+        //In Java, Comparator is an interface for sorting Java Objects
+
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
         //1st activity
         maxAct = 1;
-        ans.add(0);
+        ans.add(act[0][0]);
         //End time of 1st activity
-        int lastEnd = end[0];
+        int lastEnd = act[0][2];
 
         for(int i=0; i< end.length; i++){
-            if(start[i] >= lastEnd){
+            if(act[i][1] >= lastEnd){
                 //Select activity -> non-overlapping activity
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(act[i][0]);
+                lastEnd = act[i][2];
             }
         }
-        System.out.println("Max Activities =" +  maxAct);
+        System.out.println("Max Activities = " +  maxAct);
         for(int i=0; i<ans.size(); i++){
             System.out.print("A" + ans.get(i) + " ");
         }   
