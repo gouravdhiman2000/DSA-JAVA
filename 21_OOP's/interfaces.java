f4ffd329425776fe68356e3855dd6372896fdf28
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-06-17 23:14:37 +0530

    Interface -> implements Multiple Inheritance

--- 21_OOP's/interfaces.java
+++ 21_OOP's/interfaces.java
@@ -2,6 +2,10 @@ public class interfaces {
     public static void main(String[] args) {
         Queen q = new Queen();
         q.moves();
+
+        Bear b = new Bear();
+        b.eatMeat();
+        b.eatPlants();
     }
 }
 
@@ -24,5 +28,24 @@ class Rook implements ChessPlayer{
 class King implements ChessPlayer{
     public void moves(){
         System.out.println("up, down, left, right, diagonal in all 4 directions by 1 step");
+    }
+}
+
+//Multiple Inheritance
+interface Herbivore{
+    void eatPlants();
+}
+
+interface Carnivore{
+    void eatMeat();
+}
+
+class Bear implements Herbivore, Carnivore{
+    public void eatPlants(){
+        System.out.println("Bear eats plants");
+    }
+
+    public void eatMeat(){
+        System.out.println("Bear eats Meat");
     }
 }
