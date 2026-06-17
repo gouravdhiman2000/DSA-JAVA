89dc8176254f53dbc1b1e08ca8a721e3d81df795
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-06-17 20:36:50 +0530

    Polymorphism - Compile time Polymorphism - Method Overloading

---
+++ 21_OOP's/polymorphism.java
@@ -0,0 +1,22 @@
+public class polymorphism {
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        System.out.println(calc.sum((float)1.5, (float)2.5));
+        System.out.println(calc.sum(2, 2));
+        System.out.println(calc.sum(1, 2, 3));
+    }
+}
+
+class calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+    int sum(int a, int b,int c){
+        return a+b+c;
+    }
+
+    float sum(float a, float b){
+        return a+b;
+    }
+    
+}
