b9298c8ff66c8ce5b0f2d76147f9e4747a5a95c1
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-06-27 14:53:51 +0530

    Diameter of Binary Tree -> Approach 2

--- 22_Binary_Tree's/heightOfBinaryTree.java
+++ 22_Binary_Tree's/heightOfBinaryTree.java
@@ -37,7 +37,7 @@ public class heightOfBinaryTree {
         int rightSum = sum(root.right);
         return leftSum + rightSum + root.data;
     }
-    //diameter of BT
+    //diameter of BT - approach 1
     public static int diameter(Node root){
         if(root == null){
             return 0;
@@ -51,6 +51,30 @@ public class heightOfBinaryTree {
         int selfdia = lh + rh + 1;
 
         return Math.max(selfdia, Math.max(ld, rd));
+    }
+
+    //diameter of BT - approach 2
+    static class Info{
+        int dia;
+        int ht;
+        public Info(int dia, int ht){
+            this.dia = dia;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter2(Node root){
+        if(root == null){
+            return new Info(0,0);
+        }
+        Info leftInfo = diameter2(root.left);
+        Info rightInfo = diameter2(root.right);
+
+        int dia = Math.max(Math.max(leftInfo.dia, rightInfo.dia), leftInfo.ht + rightInfo.ht + 1);
+
+        int ht = Math.max(leftInfo.ht , rightInfo.ht) + 1;
+
+        return new Info(dia, ht);
     }
     public static void main(String[] args) {
         /*
@@ -74,6 +98,11 @@ public class heightOfBinaryTree {
 
         //System.out.println(sum(root));
 
-        System.out.println(diameter(root));
+        //System.out.println(diameter(root));
+
+        Info result = diameter2(root);
+
+        System.out.println("Diameter of Binary Tree = " + result.dia);
+        System.out.println("Height of Binary Tree = " + result.ht);
     }
 }
