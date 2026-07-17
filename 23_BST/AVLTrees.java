775abc78c5b9aa6f4e880664f2d5dd4d35ba0d1d
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-17 18:22:23 +0530

    AVL Trees Creation -> LL,RR,LR,RL Cases with Rotation

---
+++ 23_BST/AVLTrees.java
@@ -0,0 +1,120 @@
+public class AVLTrees {
+    static class Node{
+        int data, height;
+        Node left, right;
+
+        Node(int data){
+            this.data = data;
+            height = 1;
+        }
+    }
+
+    public static Node root;
+
+    public static int height(Node root){
+        if(root == null) return 0;
+
+        return root.height;
+    }
+
+    static int max(int a, int b){
+        return (a>b) ? a : b;
+    }
+
+    public static int getBalance(Node root){
+        if(root == null) return 0;
+
+        return height(root.left) - height(root.right);
+    }
+
+    public static Node insert(Node root, int key){
+        if(root == null) return new Node(key);
+
+        if(key < root.data){
+            root.left = insert(root.left, key);
+        }
+        else if(key > root.data){
+            root.right = insert(root.right, key);
+        }
+        else{
+            return root; //don't not insert - duplicates keys are not allowed
+        }
+
+        //Update root height
+        root.height = 1 + Math.max(height(root.left) , height(root.right));
+
+        //Get root's balance factor
+        int bf = getBalance(root);
+
+        //LL Case
+        if(bf > 1 && key < root.left.data){
+            return rightRotate(root);
+        }
+
+        //RR Case
+        if(bf < -1 && key > root.right.data){
+            return leftRotate(root);
+        }
+
+        //LR Case
+        if(bf > 1 && key > root.left.data){
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        //RL Case
+        if(bf < -1 && key < root.right.data){
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+        return root; //returned if AVL is balanced
+    }
+    //left Rotate subtree rooted with (x);
+    public static Node leftRotate(Node x){
+        Node y = x.right;
+        Node T2 = y.left;
+
+        //perform rotation
+        y.left = x;
+        x.right = T2;
+
+        //update heights
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left) , height(y.right)) + 1;
+
+        //return new root
+        return y;
+    }
+    //Right Rotate subtree rooted with (y)
+    public static Node rightRotate(Node y){
+        Node x = y.left;
+        Node T2 = x.right;
+
+        //perform rotation
+        x.right = y;
+        y.left = T2;
+
+        //update heights
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+
+        //Return new node
+        return x;
+    }
+    public static void preOrder(Node root){
+        if(root == null) return;
+        System.out.print(root.data + "->");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+    public static void main(String[] args) {
+        root = insert(root, 10);
+        root = insert(root, 20);
+        root = insert(root, 30);
+        root = insert(root, 40);
+        root = insert(root, 50);
+        root = insert(root, 25);
+
+        preOrder(root); // 30->20->10->25->40->50
+    }
+}
