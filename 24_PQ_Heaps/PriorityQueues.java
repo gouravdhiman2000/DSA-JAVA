8b74210987638e33b2205667d05b9f971eb4e1b8
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-07-17 23:03:08 +0530

    PQ on Objects -> Interface, function Overriding

--- 24_PQ_Heaps/PriorityQueues.java
+++ 24_PQ_Heaps/PriorityQueues.java
@@ -1,15 +1,35 @@
 import java.util.*;
 public class PriorityQueues {
+    static class Student implements Comparable<Student>{ //Interface -> overriding
+        String name;
+        int rank;
+        
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        //overriding -> parent class func. that child class also implements that function too
+        @Override 
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        //PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // pq.add(3);
+        // pq.add(4); //O(log n)
+        // pq.add(1);
+        // pq.add(7);
 
-        pq.add(3);
-        pq.add(4); //O(log n)
-        pq.add(1);
-        pq.add(7);
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 1));
+        pq.add(new Student("C", 3));
+        pq.add(new Student("D", 2));
 
         while(!pq.isEmpty()){
-            System.out.println(pq.peek()); //O(1)
+            //System.out.println(pq.peek()); //O(1)
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
             pq.remove(); //O(log n)
         }
         
