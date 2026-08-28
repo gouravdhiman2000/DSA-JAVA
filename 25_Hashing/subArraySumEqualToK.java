2523381cb975d71ad92d1be6cb4c053fe46cc76c
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2026-08-28 22:22:25 +0530

    SubArray Sum Equal to K

---
+++ 25_Hashing/subArraySumEqualToK.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class subArraySumEqualToK {
+    public static void main(String[] args) {
+        int arr[] = {10,2,-2,-20,-10};
+        int k = 10;
+        int n = arr.length;
+
+        int sum = 0;
+        int ans = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //sum, count
+        map.put(0, 1);
+
+        for(int j=0; j<n; j++){
+            sum += arr[j]; //sum[j]
+
+            if(map.containsKey(sum - k)){
+                ans += map.get(sum - k);
+            }
+
+            map.put(sum , map.getOrDefault(sum, 0) + 1);
+        }
+
+        System.out.println("No. of subArray with sum equal to k -> " + ans);
+    }
+}
